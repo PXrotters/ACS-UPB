040a09adf759a1911514e16b59dc9eb44148b6d7
PXrotters <dlmihaitalemnaru@gmail.com>
2024-06-20 16:18:23 +0300

    Tema1_PP

--- Tema1_PP/src/test/scala/TestRegression.scala
+++ Tema1_PP/src/test/scala/TestRegression.scala
@@ -8,7 +8,7 @@ class TestRegression extends FunSuite {
     val eps = 0.2 // marja de eroare de 20%
     val split = 0.1
 
-    /*
+    /* 
     Testele verifica daca functia de regresie intoarce un vector de ponderi
     care sa aproximeze cat mai bine datele de intrare.
 
@@ -24,11 +24,11 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1"), "to_predict", split, 0.1, 10)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x " +
-          "+ " + w.transpose.data.get(0)(1))
+            + w.transpose.data.get(0)(0) + "x " +
+            "+ " + w.transpose.data.get(0)(1))
         val ok = w.data.get(0)
-          .zip(List(30.13768873362556, -1.2960102570320688))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(30.13768873362556, -1.2960102570320688))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -38,11 +38,11 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1"), "to_predict", split, 0.1, 10)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x " +
-          "+ " + w.transpose.data.get(0)(1))
+            + w.transpose.data.get(0)(0) + "x " +
+            "+ " + w.transpose.data.get(0)(1))
         val ok = w.data.get(0)
-          .zip(List(62.84123952393863, -0.23918131428069359))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(62.84123952393863, -0.23918131428069359))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -52,12 +52,12 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1", "feature2"), "to_predict", split, 0.1, 1)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x1 " +
-          "+ " + w.transpose.data.get(0)(1) + "x2 " +
-          "+ " + w.transpose.data.get(0)(2))
+            + w.transpose.data.get(0)(0) + "x1 " +
+            "+ " + w.transpose.data.get(0)(1) + "x2 " +
+            "+ " + w.transpose.data.get(0)(2))
         val ok = w.data.get(0)
-          .zip(List(2.17621276701624, 6.794738067389389, 0.14722558471179611))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(2.17621276701624, 6.794738067389389, 0.14722558471179611))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -67,12 +67,12 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("GrLivArea", "YearBuilt"), "SalePrice", split, 0.01, 1)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x1 " +
-          "+ " + w.transpose.data.get(0)(1) + "x2 " +
-          "+ " + w.transpose.data.get(0)(2))
+            + w.transpose.data.get(0)(0) + "x1 " +
+            "+ " + w.transpose.data.get(0)(1) + "x2 " +
+            "+ " + w.transpose.data.get(0)(2))
         val ok = w.data.get(0)
-          .zip(List(3016079.195045662, 3564264.170022831, 1801.9062785388128))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(3016079.195045662, 3564264.170022831, 1801.9062785388128))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -82,11 +82,11 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("GrLivArea"), "SalePrice", split, 0.01, 1)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x1 " +
-          "+ " + w.transpose.data.get(0)(1))
+            + w.transpose.data.get(0)(0) + "x1 " +
+            "+ " + w.transpose.data.get(0)(1))
         val ok = w.data.get(0)
-          .zip(List(3020389.0442313547, 1802.6816438356163))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(3020389.0442313547, 1802.6816438356163))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -96,11 +96,11 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1"), "to_predict", split, 0.1, 5)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x " +
-          "+ " + w.transpose.data.get(0)(1))
+            + w.transpose.data.get(0)(0) + "x " +
+            "+ " + w.transpose.data.get(0)(1))
         val ok = w.data.get(0)
-          .zip(List(18.15636490729292, -1.0355229938376973))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(18.15636490729292, -1.0355229938376973))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -110,11 +110,11 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1"), "to_predict", split, 0.01, 5)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x " +
-          "+ " + w.transpose.data.get(0)(1))
+            + w.transpose.data.get(0)(0) + "x " +
+            "+ " + w.transpose.data.get(0)(1))
         val ok = w.data.get(0)
-          .zip(List(4.877868348580691, -0.03016982434403231))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(4.877868348580691, -0.03016982434403231))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -124,12 +124,12 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("feature1", "feature2"), "to_predict", split, 0.001, 5)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x1 " +
-          "+ " + w.transpose.data.get(0)(1) + "x2 " +
-          "+ " + w.transpose.data.get(0)(2))
+            + w.transpose.data.get(0)(0) + "x1 " +
+            "+ " + w.transpose.data.get(0)(1) + "x2 " +
+            "+ " + w.transpose.data.get(0)(2))
         val ok = w.data.get(0)
-          .zip(List(0.10850027614878494, 0.33897039511907456, 0.007331630833966705))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(0.10850027614878494, 0.33897039511907456, 0.007331630833966705))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
@@ -139,12 +139,12 @@ class TestRegression extends FunSuite {
         val (w, loss) = Regression.regression(
             file, List("GrLivArea", "YearBuilt"), "SalePrice", split, 0.001, 5)
         println("\t" + "y = "
-          + w.transpose.data.get(0)(0) + "x1 " +
-          "+ " + w.transpose.data.get(0)(1) + "x2 " +
-          "+ " + w.transpose.data.get(0)(2))
+            + w.transpose.data.get(0)(0) + "x1 " +
+            "+ " + w.transpose.data.get(0)(1) + "x2 " +
+            "+ " + w.transpose.data.get(0)(2))
         val ok = w.data.get(0)
-          .zip(List(4.532065772931244E20, 5.6503657069136924E20, 2.86489323160482368E17))
-          .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
+            .zip(List(4.532065772931244E20, 5.6503657069136924E20, 2.86489323160482368E17))
+            .map((a, b) => a / b < 1 + eps && a / b > 1 - eps)
         println("Loss: " + loss)
         assert(ok.reduce(_ && _))
     }
